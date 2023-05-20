aef19ff90dd664dab1d273c641c31e61634ea48e
mbroutin <mbroutin@sedona.fr>
2023-05-20 21:34:34 +0200

    feat: use redis as simple example

--- examples/simple-session/src/hooks.server.ts
+++ examples/simple-session/src/hooks.server.ts
@@ -1,15 +1,14 @@
-import { InMemorySessionRepository, handleSession } from '@sveltekit-statefull-session/core';
+import { handleSession } from '@sveltekit-statefull-session/core';
 import { RedisSessionRepository } from '@sveltekit-statefull-session/repository-redis';
+import { REDIS_URL } from '$env/static/private';
 
 import { createClient } from '@redis/client';
 
-// const redis = createClient({
-//     url: 'redis://localhost:32768/0'
-// });
-// redis.connect();
-// export const handle = handleSession(new RedisSessionRepository({ ttl: '60s', client: redis }), { debug: true })
-
-console.log(RedisSessionRepository);
+const redis = createClient({
+    url: REDIS_URL
+});
+await redis.connect();
+export const handle = handleSession(new RedisSessionRepository({ ttl: '60s', client: redis }), { debug: true })
 
 // OR
-export const handle = handleSession(new InMemorySessionRepository({ ttl: '60s' }), { debug: true })
+// export const handle = handleSession(new InMemorySessionRepository({ ttl: '60s' }), { debug: true })
