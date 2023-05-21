dac04522f4c67cd096100dc536f5b51b6b583ed9
mbroutin <mbroutin@sedona.fr>
2023-05-21 16:12:03 +0200

    feat: add tls support

--- examples/simple-session/src/hooks.server.ts
+++ examples/simple-session/src/hooks.server.ts
@@ -1,11 +1,14 @@
 import { handleSession } from '@sveltekit-statefull-session/core';
 import { RedisSessionRepository } from '@sveltekit-statefull-session/repository-redis';
-import { REDIS_URL } from '$env/static/private';
+import { REDIS_URL, REDIS_URL_TLS } from '$env/static/private';
 
 import { createClient } from '@redis/client';
 
 const redis = createClient({
-    url: REDIS_URL
+    url: REDIS_URL,
+    socket: {
+        tls: REDIS_URL_TLS === "true"
+    }
 });
 await redis.connect();
 export const handle = handleSession(new RedisSessionRepository({ ttl: '60s', client: redis }), { debug: true })
